8a9a52dc49a440341448f4e01bbbb3f078c89e2e
MBrosik <michal.bros1@gmail.com>
2024-12-13 21:09:52 +0100

    set up shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 
 import styles from "./styles.module.scss";
+import {Button, buttonVariants} from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className={styles['text-test']}>Nice text</div>
+      <Button className={buttonVariants({variant: 'destructive'})}>Click me</Button>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
