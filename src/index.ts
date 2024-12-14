0cd28484c640688519fb1c5046ffa7e08df5fb66
MBrosik <michal.bros1@gmail.com>
2024-12-14 15:28:35 +0100

    simple express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello world")
+import bodyParser from "body-parser";
+import express from "express";
+
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+app.listen(port, () => console.info(`start serwera na porcie ${port}`))
